f19aa1ab6added904cea166c78f2e1c0090b115b
Stevie Frederick <leflor@gmail.com>
2021-11-01 20:56:10 -0400

    assign user to ticket on creation with or without quotes (untested)

--- application/constants/kanbot-commands.ts
+++ application/constants/kanbot-commands.ts
@@ -40,39 +40,62 @@ export class KanbotRequest implements KanbotRequest {
 
     constructor(command: KanbotCommands, taskName: string, additionalArgs: string) {
         this.command = command;
-	this.taskName = taskName;
-	this.additionalArgs = additionalArgs;
+	      this.taskName = taskName;
+	      this.additionalArgs = additionalArgs; //ideally can be split by spaces
     }
 
+    // FIXME: Right now we assume the only argument in quotes would be the taskname
+    //        thus all other arguments past next to it not in quotes will be "singular"
+    //        in future need clean up command parsing to be more similar to configuration as
+    //        something like argparse in python
+
+    // TODO: Replace with argparser or expand into a custom argparser
     public static parseString(input: string): KanbotRequest {
         // split on first space - won't work if we allow commands to have multiple arguments
         const spaceIndex: number = input.indexOf(' ');
         const command: KanbotCommands = getKanbotCommand(input.substring(0, spaceIndex));
-	//const taskName: string = `${trim(input.substring(spaceIndex + 1, input.length), '"')}`;
+	      //const taskName: string = `${trim(input.substring(spaceIndex + 1, input.length), '"')}`;
 
         const optArgvRaw = input.match(/".+?"/g);
         var optArgv = null;
+        var remainingOptArgs = "";
+        var remainingOptArgv = null
+
+        //get args in quotes then remove them from the arg string
         if( optArgvRaw != null ){
             optArgv = optArgvRaw.map(str => str.replace(/"/g, ''));
+            if(optArgv != null ){
+                //we assume 1 and only 1 argument in quotes will ever exist
+                if( optArgv[0] != null ){ //starting to bug me but necessary
+                  // arguments that are remaining to be mutexed with
+                  remainingOptArgs = input.replace(optArgv[0], "").replace(input.substring(0, spaceIndex), "");
+                }
+            }
+            console.log(`remaining argv: ${remainingOptArgs}`);
             console.log(`Opt argv ${optArgvRaw} and ${optArgv[0]}`);
         }
 
+        //when we fill this it will be mutually exclusive
         var tmp_taskName: string = "";
         var tmp_additionalArgs: string = "";
 
-	if( optArgv != null){
-            if( optArgv[0] != null ){
-	        tmp_taskName = `${trim(optArgv[0], '"')}`;
-            console.log(`taskname ${tmp_taskName}`);
-	    }
-            if( optArgv[1] != null ){
-	        tmp_additionalArgs = `${trim(optArgv[1], '"')}`;
-            console.log(`additionalArgs ${tmp_additionalArgs}`);
-	    }
+        if( remainingOptArgs != null ){
+          tmp_additionalArgs = remainingOptArgs;
         }
 
-	const taskName: string = tmp_taskName;
-	const additionalArgs: string = tmp_additionalArgs;
+        if( optArgv != null){
+            if( optArgv[0] != null && (tmp_taskName == ""|| tmp_additionalArgs == null)){
+                 tmp_taskName = `${trim(optArgv[0], '"')}`;
+                 console.log(`taskname ${tmp_taskName}`);
+            }
+            if( optArgv[1] != null ){
+                 tmp_additionalArgs = `${trim(optArgv[1], '"')}`;
+                 console.log(`additionalArgs ${tmp_additionalArgs}`);
+            }
+        }
+
+      	const taskName: string = tmp_taskName;
+      	const additionalArgs: string = tmp_additionalArgs;
         return new KanbotRequest(command, taskName, additionalArgs);
     }
 }
